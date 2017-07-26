4f3b84a53a14f1c8d18d4568bc1ded03bab21414
Mati <mev13@o2.pl>
2017-07-26 18:02:10 +0200

    Console App added. Just for fun.

---
+++ FizzBuzzConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using FizzBuzzLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fb = new FizzBuzz();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(fb.CheckNumber(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
