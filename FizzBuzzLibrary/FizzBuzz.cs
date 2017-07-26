502540d04231238e29c5942e84be10d7826f0480
Mati <mev13@o2.pl>
2017-07-26 17:56:35 +0200

    FizzBuzz Library created

--- FizzBuzzLibrary/FizzBuzz.cs
+++ FizzBuzzLibrary/FizzBuzz.cs
@@ -7,6 +7,34 @@ namespace FizzBuzzLibrary
 {
     public class FizzBuzz
     {
+        public FizzBuzz()
+        {
 
+        }
+
+        /// <summary>
+        /// Check any number is divisible by 3 and 5
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string CheckNumber(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "Fizz Buzz";
+            }
+
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+        }
     }
 }
